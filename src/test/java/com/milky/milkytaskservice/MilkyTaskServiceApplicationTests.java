ca4cea1133cc33b9ac3f85de04156991788e65c3
Gizelly Silva <gizelly.silva@riachuelo.com.br>
2023-09-26 20:44:06 -0300

    feat: initial commit

---
+++ src/test/java/com/milky/milkytaskservice/MilkyTaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milky.milkytaskservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkyTaskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
