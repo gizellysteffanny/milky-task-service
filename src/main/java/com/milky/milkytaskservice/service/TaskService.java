fe801e50712e58456f1f55b2ae834801558db521
Gizelly Steffanny <gizellysteffanny@gmail.com>
2023-10-04 23:01:44 -0300

    feat: add update method in task service
    - create a validator
    - create a handler exception to record not found
    - create a handler exception to task invalids

--- src/main/java/com/milky/milkytaskservice/service/TaskService.java
+++ src/main/java/com/milky/milkytaskservice/service/TaskService.java
@@ -1,9 +1,13 @@
 package com.milky.milkytaskservice.service;
 
+import com.milky.milkytaskservice.exception.RecordNotFoundException;
+import com.milky.milkytaskservice.exception.TaskException;
 import com.milky.milkytaskservice.model.Task;
+import com.milky.milkytaskservice.validator.TaskUserValidator;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -14,18 +18,18 @@ import com.milky.milkytaskservice.repository.TaskRepository;
 
 @Validated
 @Slf4j
+@AllArgsConstructor
 @Service
 public class TaskService {
   private final TaskRepository taskRepository;
   private final TaskMapper taskMapper;
-  
-  public TaskService(TaskRepository taskRepository, TaskMapper taskMapper) {
-    this.taskRepository = taskRepository;
-    this.taskMapper = taskMapper;
-  }
+  private final TaskUserValidator taskUserValidator;
 
   public TaskDTO create(@Valid TaskDTO taskDTO, @NotNull @NotBlank String user) {
     log.info("Creating task from user({}): {}", user, taskDTO);
+
+    taskUserValidator.userCanCreateTask(user);
+
     Task task = taskMapper.toEntity(taskDTO);
     task.setUser(user);
 
@@ -33,4 +37,20 @@ public class TaskService {
     log.info("Task created: {}", savedTask);
     return taskMapper.toDTO(savedTask);
   }
+
+  public TaskDTO update(@Valid TaskDTO taskDTO, @NotNull @NotBlank String id, @NotNull @NotBlank String user) {
+    log.info("Updating task from user({}): {}", user, taskDTO);
+    Task task = taskRepository.findById(id).orElse(null);
+
+    if (task == null) {
+      throw new RecordNotFoundException("Task not found");
+    }
+
+    taskUserValidator.userCanUpdateTask(user, task.getUser());
+
+    Task savedTask = taskRepository.save(taskMapper.toEntity(taskDTO));
+    log.info("Task updated: {}", savedTask);
+    return taskMapper.toDTO(savedTask);
+  }
+
 }
--- src/test/java/com/milky/milkytaskservice/service/TaskServiceTest.java
+++ src/test/java/com/milky/milkytaskservice/service/TaskServiceTest.java
@@ -1,5 +1,8 @@
 package com.milky.milkytaskservice.service;
 
+import com.milky.milkytaskservice.exception.RecordNotFoundException;
+import com.milky.milkytaskservice.exception.TaskException;
+import com.milky.milkytaskservice.validator.TaskUserValidator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +12,7 @@ import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 import java.util.Date;
@@ -30,6 +34,9 @@ public class TaskServiceTest {
     @Mock
     private TaskMapper taskMapper;
 
+    @Mock
+    private TaskUserValidator taskUserValidator;
+
     @Test
     @DisplayName("should be able to create a task")
     public void createTask() {
@@ -48,9 +55,103 @@ public class TaskServiceTest {
                 .build();
 
         when(taskRepository.save(any())).thenReturn(task);
+        when(taskMapper.toEntity(any())).thenReturn(task);
         when(taskMapper.toDTO(any())).thenReturn(taskDTO);
         TaskDTO createdTask = taskService.create(taskDTO, user);
 
         Assertions.assertEquals(taskDTO.getTitle(), createdTask.getTitle());
+    }
+
+    @Test
+    @DisplayName("should not be able to create a task without a user")
+    public void createTaskWithoutUser() {
+        String user = "";
+        TaskDTO taskDTO = TaskDTO.builder()
+                .title("task title")
+                .description("task description")
+                .completed(false)
+                .build();
+
+        doThrow(new TaskException("User not allowed to create task"))
+                .when(taskUserValidator).userCanCreateTask(any());
+
+        assertThrows(TaskException.class, () -> {
+            taskService.create(taskDTO, user);
+        });
+    }
+
+    @Test
+    @DisplayName("should be able to update a task")
+    public void updateTask() {
+        String user = "1";
+        String id = "1";
+        TaskDTO taskDTO = TaskDTO.builder()
+                .id(id)
+                .title("task title")
+                .description("task description")
+                .completed(false)
+                .build();
+
+        Task task = Task.builder()
+                .title("task title")
+                .description("task description")
+                .completed(false)
+                .createdAt(new Date())
+                .user(user)
+                .build();
+
+        when(taskRepository.findById(any())).thenReturn(java.util.Optional.of(task));
+        when(taskRepository.save(any())).thenReturn(task);
+        when(taskMapper.toDTO(any())).thenReturn(taskDTO);
+        TaskDTO updatedTask = taskService.update(taskDTO, id, user);
+
+        Assertions.assertEquals(taskDTO.getTitle(), updatedTask.getTitle());
+    }
+
+    @Test
+    @DisplayName("should not be able to update a task from another user")
+    public void updateTaskFromAnotherUser() {
+        String user = "1";
+        String id = "1";
+        TaskDTO taskDTO = TaskDTO.builder()
+                .id(id)
+                .title("task title")
+                .description("task description")
+                .completed(false)
+                .build();
+
+        Task task = Task.builder()
+                .title("task title")
+                .description("task description")
+                .completed(false)
+                .createdAt(new Date())
+                .user("2")
+                .build();
+
+        when(taskRepository.findById(any())).thenReturn(java.util.Optional.of(task));
+        doThrow(new TaskException("User not allowed to update this task"))
+                .when(taskUserValidator).userCanUpdateTask(any(), any());
+
+        assertThrows(TaskException.class, () -> {
+            taskService.update(taskDTO, id, user);
+        });
+    }
+
+    @Test
+    @DisplayName("should not be able to update a task that does not exist")
+    public void updateTaskThatDoesNotExist() {
+        String user = "1";
+        String id = "1";
+        TaskDTO taskDTO = TaskDTO.builder()
+                .id(id)
+                .title("task title")
+                .description("task description")
+                .completed(false)
+                .build();
+
+        when(taskRepository.findById(any())).thenReturn(java.util.Optional.empty());
+        assertThrows(RecordNotFoundException.class, () -> {
+            taskService.update(taskDTO, id, user);
+        });
     }
 }
